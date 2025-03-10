8004e183ad6e235d21034496e70f748b8693204c
Flufzak <khachatryan.suzi@gmail.com>
2025-03-10 12:07:52 +0100

    cijfers nu afgewerkt

--- cijfers.js
+++ cijfers.js
@@ -1,17 +1,17 @@
 const API_URL = "http://localhost:3000/cijfers";
 
-// Fetch and display products
+// Fetch en toon de cijfers
 if (window.location.pathname.includes("cijfers.html")) {
     fetch(API_URL)
-        .then(res => res.json())
-        .then(cijfer => {
+        .then(res => res.json())  // Zet de JSON-response om naar een JavaScript-object
+        .then(cijfers => {
             const list = document.getElementById("cijfer-lijst");
-            products.forEach(product => {
+            cijfers.forEach(cijfer => {   // Voor elk cijfer in de lijst
                 const li = document.createElement("li");
-                li.innerHTML = `<a href="details.html?id=${cijfer.id}">${cijfer.name}</a>`;
+                // Maak een link naar de detailpagina voor elk cijfer
+                li.innerHTML = `<a href="cijferdetails.html?id=${cijfer.id}">${cijfer.nummer}</a>`;
                 list.appendChild(li);
             });
         })
-        .catch(err => console.error("Error cijfers:", err));
+        .catch(err => console.error("Error cijfers:", err)); // Foutmelding als de fetch mislukt
 }
-
